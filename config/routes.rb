45d476a4f73e1ed19d0dce7a4ef513cec8b7a6e1
Anibal De La O <adelaome@uncc.edu>
2017-04-05 15:09:32 +0000

    Login Page Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :villains
   root 'welcome#index'
   
+  get 'signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
