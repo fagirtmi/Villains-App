c196f9b402e9c3c6e71927e7b43a0a475084dace
Anibal De La O <adelaome@uncc.edu>
2017-04-05 16:12:06 +0000

    Nav bar maintence

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
    end
    
    def create
-  @user = User.find_by_email(params[:session][:email])
-  if @user && @user.authenticate(params[:session][:password])
-    session[:user_id] = @user.id
-    redirect_to '/'
-  else
-    redirect_to 'login'
-  end 
-end
+      @user = User.find_by_email(params[:session][:email])
+      if @user && @user.authenticate(params[:session][:password])
+       session[:user_id] = @user.id
+       redirect_to '/'
+      else
+       redirect_to 'login'
+      end 
+   end
 end
