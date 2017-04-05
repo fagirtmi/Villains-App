01639dbb73e515fe44728c2437a958b5a9689c4b
Anibal De La O <adelaome@uncc.edu>
2017-04-05 15:52:58 +0000

    format color to whatever tab user is on

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+   def new
+   end
+   
+   def create
+  @user = User.find_by_email(params[:session][:email])
+  if @user && @user.authenticate(params[:session][:password])
+    session[:user_id] = @user.id
+    redirect_to '/'
+  else
+    redirect_to 'login'
+  end 
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   
   get 'signup' => 'users#new'
   resources :users
+  
+  get 'login' => 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
