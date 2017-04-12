6f26e28fa7ee4946854cd7ab0219719c3421022c
Anibal De La O <adelaome@uncc.edu>
2017-04-12 04:36:10 +0000

    Added Show method in villains controller

--- app/controllers/villains_controller.rb
+++ app/controllers/villains_controller.rb
@@ -3,8 +3,27 @@ class VillainsController < ApplicationController
       @villain = Villain.new
    end
    def create
-      @villain = Villain.new()
+      @villain = Villain.new(villain_params)
       if @villain.save
-         
+         redirect_to @villain
+      else
+         render 'new'
+      end
    end
+   def show
+      @villain = Villain.find(params[:id])
+   end
+   
+   
+   private
+   def villain_params
+       params.require(:villain).permit(:firstName, :lastName, :alias, :birthday, :gender,
+       :rank, :cursed, :super_strength, :healing_factor, :invulnerability, :duplication,
+       :dynamic_camouflage, :invisibility, :enhanced_awarness, :enhanced_durability, 
+       :enhanced_marksmanship, :enhanced_reflexes, :enhanced_senses, :clairvoyance, 
+       :energy_manipulation, :immortality, :mental_projection, :psychic, 
+       :superhuman_intelligence, :time_travel, :telepathy, :teleportation, :searching_for)  
+   end
+   
 end
+
